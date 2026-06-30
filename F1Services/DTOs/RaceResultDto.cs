12352f8dc39d8a3866e46ffa60c36d04bedb848a
Jente Driesen <jente-driesen@hotmail.com>
2026-06-30 18:37:09 +0200

    Added RaceResultDto

---
+++ F1Services/DTOs/RaceResultDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1Services.DTOs;
+
+public class RaceResultDto
+{
+    public int Position { get; set; }
+    public string Driver { get; set; }
+    public string Team { get; set; }
+    public string Time { get; set; }
+    public decimal Points { get; set; }
+    public bool FastestLap { get; set; }
+}
